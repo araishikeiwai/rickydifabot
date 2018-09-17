1dc90c8ac837ca12e596c3935566da5599b80c19
Rick Daniel <rick@araishikeiwai.com>
2018-09-17 11:26:34 +0700

    new toll tariffs

--- lib/ricky_difa_bot/expense_queue.rb
+++ lib/ricky_difa_bot/expense_queue.rb
@@ -3,11 +3,14 @@ class RickyDifaBot::ExpenseQueue
 
   KEYBOARDS = [
     [
+      ['T45R', 'T45T'],
       ['T65R', 'T65T'],
       ['T7R', 'T7T'],
       ['T95R', 'T95T'],
+      ['T15R', 'T15T'],
       ['T16R', 'T16T'],
-      ['T165R', 'T165T']
+      ['T165R', 'T165T'],
+      ['T215R', 'T215T']
     ],
     [
       ['Busway Flazz', 'KAI Flazz'],
