af5c6adb74f354ab9aef9f7c628cd88279390795
Rick Daniel <rick@araishikeiwai.com>
2020-02-28 18:08:41 -0500

    better summary

--- lib/ricky_difa_bot/timeline.rb
+++ lib/ricky_difa_bot/timeline.rb
@@ -27,16 +27,33 @@ class RickyDifaBot::Timeline
 
   def self.summary
     prints = []
-    prints <<
-      [
-        "Date",
-        fields.except('_id', 'date').keys.map do |action|
-          "#{action}\t\t\t"
-        end.join("\t")
-      ].join("\t")
+    prints2 = []
 
-    self.all.each do |tl|
+    hasht = {
+      go: [],
+      work_start: [],
+      work_finished: [],
+      home: []
+    }
+    hash2 = {
+      wd: [],
+      gd: [],
+      hd: [],
+      tc: []
+    }
+
+    #prints <<
+    #  [
+    #    "Date",
+    #    fields.except('_id', 'date').keys.map do |action|
+    #      "#{action}\t\t\t"
+    #    end.join("\t")
+    #  ].join("\t")
+
+    self.where(:date.gte => Date.new(2020, 1, 1)).each do |tl|
       print = [tl.date.strftime("%d/%m/%Y")]
+
+      cur = {}
 
       fields.except('_id', 'date').keys.each do |action|
         if time = tl.send(action)
@@ -48,14 +65,56 @@ class RickyDifaBot::Timeline
           print << ":"
           print << time.min
           print << time.hour * 60 + time.min
+          hasht[action.to_sym] << time.hour * 60 + time.min
+          hasht[action.to_sym].sort!
+          cur[action.to_sym] = time.hour * 60 + time.min
         else
           print << ([""] * 4).flatten
         end
       end
 
+      hasht.keys.each do |action|
+        size = hasht[action].size
+        av = hasht[action].sum / size
+        me = size % 2 == 0 ? ((hasht[action][size / 2 - 1] + hasht[action][size / 2]) / 2).to_i : hasht[action][size / 2]
+        gopr(print, av)
+        gopr(print, me)
+      end
+
       prints << print.join("\t")
+      print = []
+
+      wd = cur[:work_finished] - cur[:work_start] rescue 0
+      gd = cur[:work_start] - cur[:go] rescue 0
+      hd = cur[:home] - cur[:work_finished] rescue 0
+      tc = gd + hd
+
+      gopr(print, wd)
+      gopr(print, gd)
+      gopr(print, hd)
+      gopr(print, tc)
+
+      hash2.keys.each do |action|
+        hash2[action] << eval(action.to_s)
+        hash2[action].sort!
+
+        size = hash2[action].size
+        av = hash2[action].sum / size
+        me = size % 2 == 0 ? ((hash2[action][size / 2 - 1] + hash2[action][size / 2]) / 2).to_i : hash2[action][size / 2]
+        gopr(print, av)
+        gopr(print, me)
+      end
+
+      prints2 << print.join("\t")
     end
 
-    prints.join("\n")
+    prints.join("\n") + "\n\n\n" + prints2.join("\n")
+  end
+
+  def self.gopr(print, uu)
+    print << uu / 60
+    print << ':'
+    print << uu % 60
+    print << uu
   end
 end
